46b782fe656b676c2bb095f3bf370ae61c8761a1
Airmid <forsa9828@gmail.com>
2020-05-19 01:15:48 -0500

    deploying

---
+++ build/precache-manifest.4f6927fe741ad8698ff235494198cdfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fba559c14632a00b83605d6496a9a57a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66cb5d551ade6d83a490",
+    "url": "/static/css/main.dc41f2b6.chunk.css"
+  },
+  {
+    "revision": "9db871c694945a8140a6",
+    "url": "/static/js/2.b2b1e642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b2b1e642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66cb5d551ade6d83a490",
+    "url": "/static/js/main.c85a42a5.chunk.js"
+  },
+  {
+    "revision": "2de6a86b6e46165a2f07",
+    "url": "/static/js/runtime-main.8a356cd9.js"
+  }
+]);
